9d16d3585cce4caee53aae500d2199c855426189
Aarni Koskela <akx@iki.fi>
2017-12-20 15:18:57 +0200

    Refactor raster out

--- drawbdf.js
+++ drawbdf.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const parseBdf = require('./lib/parse-bdf');
 const drawText = require('./lib/draw');
+const rasterizeToLines = require('./lib/raster');
 if (process.argv.length <= 2) {
   console.error(`usage: ${process.argv[1]} bdf-file.bdf "text here"`);
   process.exit(1);
@@ -8,12 +9,7 @@ if (process.argv.length <= 2) {
 const fontData = parseBdf(fs.readFileSync(process.argv[2], 'UTF-8'));
 const text = process.argv[3] || 'Hi';
 const res = drawText(fontData, text);
-const [minX, minY, maxX, maxY] = res.bbox;
-const buffer = new Set(res.pixels.map(([x, y]) => `${x},${y}`));
-for (var y = minY; y <= maxY; y++) {
-  const line = [];
-  for (var x = minX; x <= maxX; x++) {
-    line.push(buffer.has(`${x},${y}`) ? '#' : ' ');
-  }
-  console.log(line.join(''));
-}
+const lines = rasterizeToLines(res, ' ', '#');
+lines.forEach(line => {
+  console.log(line.join(' '));
+});
